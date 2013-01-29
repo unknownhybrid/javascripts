dcefeeba7172d42660364559655e3ac344231509
unknownhybrid <unknownhybrid@gmail.com>
2013-01-29 11:21:45 -0600

    Update auburn.user.js
    Adding functionality to Clay's javascript to let us shorthand some URLs.

--- auburn.user.js
+++ auburn.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name		Auburn Link Fixer
 // @version		0.2
-// @description	Automatically uses https:// on all fp.auburn.edu sites. Toggles cws/www at 404s.
+// @description	Automatically uses https:// on all fp.auburn.edu sites. Toggles cws/www at 404s. Allows for
+//                  [dev, test, migr, cws, fp, auburn]/<PROJECT_NAME> redirects.
 // @match		*://*auburn.edu*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
-// @copyright	Clay Miller (clay@smockle.com)
+// @copyright	Clay Miller (clay@smockle.com), Jake Harris (javakatdesign@gmail.com)
 // ==/UserScript==
 if (~location.href.indexOf("http://fp.auburn.edu")) window.location = "https://" + window.location.hostname + window.location.pathname;
+if (~location.href.indexOf("fp/")) window.location = "https://fp.auburn.edu/" + window.location.pathname;
 $(document).ready(function() {
     $.ajax({
         url: location.href,
